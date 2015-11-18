e60d3b6639e2724327b4805c602b6d7916a8007b
Aditya Kapoor <adityakapoor.mait@gmail.com>
2015-11-18 21:40:42 +0530

    add user approve/disapprove from admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,13 +1,30 @@
 ActiveAdmin.register User do
   permit_params :first_name, :last_name, :email, :username
 
+  member_action :approve, method: :patch do
+    resource.update_column(:approved, true)
+    redirect_to admin_users_path, notice: 'Approved!'
+  end
+
+  member_action :disapprove, method: :patch do
+    resource.update_column(:approved, false)
+    redirect_to admin_users_path, notice: 'Disapproved'
+  end
+
   index do
     id_column
     column :first_name
     column :last_name
     column :email
     column :username
-    actions
+    column :approved
+    actions defaults: true do |user|
+      if user.approved?
+        link_to 'Disapprove', disapprove_admin_user_path(user), method: :patch
+      else
+        link_to 'Approve', approve_admin_user_path(user), method: :patch
+      end
+    end
   end
 
   filter :first_name
@@ -21,6 +38,8 @@ ActiveAdmin.register User do
       f.input :last_name
       f.input :email
       f.input :username
+      f.input :classroom
+      f.input :role
     end
     f.actions
   end
