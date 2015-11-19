4d7318f682117b691dfcc1bcafb106be761644fc
Aditya Kapoor <adityakapoor.mait@gmail.com>
2015-11-19 23:27:37 +0530

    Add user avatarsgs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,30 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :load_user, only: [:edit, :update]
 
   def dashboard
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(update_params)
+      redirect_to dashboard_path, notice: 'User Updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def load_user
+      @user = User.where(id: params[:id]).first
+      redirect_to(root_path, error: 'User Not Found') unless @user
+    end
+
+    def update_params
+      params.require(:user)
+            .permit(:email, :first_name, :last_name, :avatar)
+    end
 end
