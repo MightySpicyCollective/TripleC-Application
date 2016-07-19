8a5972347a234d5cc5355f776b5a2ac39c4f10ac
Aditya Kapoor <adityakapoor.mait@gmail.com>
2016-07-19 10:41:55 +0530

    it should be action_mailer not controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_controller.default_url_options = {
+  config.action_mailer.default_url_options = {
     host: 'go.triplec.global'
   }
 end
